6fcfc8456d706afbff9749fcbff964bdf8894113
vanshika704 <sharmavanshi704@gmail.com>
2023-12-16 10:00:00 +0530

    c++add

---
+++ addition.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){ int amount1;int amount2;int sum;
+    cout<<"enter amount1";
+    cin>> amount1;
+    cout<<"enter amount2";
+    cin>> amount2;
+    sum=amount1+amount2;
+    return 0;}
+
