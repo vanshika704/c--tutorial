ce9d9efff8f8629887d46627909dfd0906aec3de
vanshika704 <sharmavanshi704@gmail.com>
2024-01-05 10:00:00 +0530

    c++add

---
+++ breakandcontinue.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){int number;
+    cout<<"enter number";
+    cin>>number;
+    for(number=0;number<100;number++){
+        if(number%number==0,number%1==0){
+            cout<<"prime number";
+            continue;
+
+        }else(cout<<"not a prime number");
+    }
+}
