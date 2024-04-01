f35595ffa73ac796a6c789ec62a3aeae410fecdd
vanshika704 <sharmavanshi704@gmail.com>
2024-04-01 10:00:00 +0530

    c++add

---
+++ pattern3.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int rows,i,j;
+    cout<< "enter number";
+ cin>>rows;
+ for(i=1;i<=rows;i++){
+    for(j=1;j<=i;j++){
+        cout<<"*" <<" ";
+    
+    }cout<<endl;
+ }
+
+}
