4fd14ac87b431daca71ae93cf856008b3b7c399f
vanshika704 <sharmavanshi704@gmail.com>
2024-04-01 10:00:00 +0530

    c++add

---
+++ pattern2.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int i=1,j=1,count=1,k;
+    cout<<"enter number";
+    cin>>j;
+   
+    for(i=1;i<=j;i++){
+        for(k=1;k<=j;k++){
+           
+            cout<<count<<" ";
+             count=count+1;
+        }cout<<endl;
+}
+    }
+
