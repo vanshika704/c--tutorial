3930c30983a562aa5ce1a186c5808c76e690c168
vanshika704 <sharmavanshi704@gmail.com>
2024-04-05 10:00:00 +0530

    c++add

---
+++ whileloop.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){int number;
+    cout<<"enter number";
+    cin>>number;
+    while(number%2==0){
+        cout<<"even number";
+    }
+    return 0;
+}
