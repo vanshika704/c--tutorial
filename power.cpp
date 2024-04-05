fc0837a69ddb4488f20237b8b92daeacd05c46b0
vanshika704 <sharmavanshi704@gmail.com>
2024-04-05 10:00:00 +0530

    c++add

---
+++ power.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include <math.h>
+using namespace std;
+int power(int a,int b );
+
+int main(){
+    int a,b;
+    cout<<" enter a";
+    cin>> a;
+    cout<<" enter b";
+    cin>> b;
+   
+    cout<< result<< endl;
+}
+
+
+
+
