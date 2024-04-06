35e47ee589302c9855d3c165d1f6a8cb75223d3b
vanshika704 <sharmavanshi704@gmail.com>
2024-04-06 10:00:00 +0530

    c++add

---
+++ factorialfunctions.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std ;
+int factorial(int number){
+    for(int i=2;i<=number;i++){
+       int factor= factor*i;
+    }
+
+}
+int main(){int number=7;
+  int result= factorial(number);
+    
+
+cout << result << endl;}
