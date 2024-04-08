59fb2cd415896822a79eac601ae6fbdc8b624668
vanshika704 <sharmavanshi704@gmail.com>
2024-04-08 10:00:00 +0530

    c++add

---
+++ loops.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int number;
+    cout<<"enter number";
+    cin>> number;
+    for(int i=0;i<=number;i++){
+        cout<<i;
+    }
+    return 0;
+}
